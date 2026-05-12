bce2dce721aeacc7a5dc6aed0b7ce8d3c74f9c93
Philip Hjalmrud Ekströmer <p.ekstromer@gmail.com>
2026-05-12 13:43:39 +0200

    fix: fix typing

--- src/data/rooms.ts
+++ src/data/rooms.ts
@@ -1,23 +1,22 @@
 // src/data/routes.ts
 
 
-export const ROOM_DATA_STRUCTURE = {
+export const ROOM_DATA_STRUCTURE: Record<string, Record<string, string[]>> = {
   "Huvudentrén": {
   "Spinnhuset": [
       "Gå in genom huvudentrén på Plan 1.",
       "Ta hissen (rakt fram till höger) upp till Plan 4.",
       "Gå ut ur hissen och ta höger genom den grå branddörren.",
       "Sväng första vänster vid fikarummet.",
-      "Spannhuset är den andra dörren på höger sida."
+      "Spinnhuset är den andra dörren på höger sida."
     ]
   },
     "Personalingång lastkajen": {
     "Spinnhuset": [
-      "Gå in genom personalingången på Plan 1.",
-      "Ta hissen (rakt fram till vänster) upp till Plan 4.",
-      "Gå ut ur hissen och ta vänster genom den grå branddörren.",
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå höger mot fikarummet.",
       "Sväng första vänster vid fikarummet.",
-      "Spannhuset är den andra dörren på höger sida."
+      "Spinnhuset är den andra dörren på höger sida."
     ]}
 
   }
