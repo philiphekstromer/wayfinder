b9aa3b62624f8f05800835c4c39085616c029c20
Philip Hjalmrud Ekströmer <p.ekstromer@gmail.com>
2026-05-12 13:27:13 +0200

    fix: change data structure to entryways and then rooms

--- src/data/rooms.ts
+++ src/data/rooms.ts
@@ -1,16 +1,9 @@
 // src/data/routes.ts
 
-export interface RoomRoute {
-  steps: string[];
-}
 
-export interface RoomData {
-  [key: string]: RoomRoute;
-}
-
-export const OFFICE_ROOMS: RoomData = {
-  "Spannhuset": {
-    steps: [
+export const ROOM_DATA_STRUCTURE = {
+  "Huvudentrén": {
+  "Spinnhuset": [
       "Gå in genom huvudentrén på Plan 1.",
       "Ta hissen (rakt fram till höger) upp till Plan 4.",
       "Gå ut ur hissen och ta höger genom den grå branddörren.",
@@ -18,5 +11,13 @@ export const OFFICE_ROOMS: RoomData = {
       "Spannhuset är den andra dörren på höger sida."
     ]
   },
+    "Personalingång lastkajen": {
+    "Spinnhuset": [
+      "Gå in genom personalingången på Plan 1.",
+      "Ta hissen (rakt fram till vänster) upp till Plan 4.",
+      "Gå ut ur hissen och ta vänster genom den grå branddörren.",
+      "Sväng första vänster vid fikarummet.",
+      "Spannhuset är den andra dörren på höger sida."
+    ]}
 
-};
+  }
