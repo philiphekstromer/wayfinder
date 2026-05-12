31c1e81e7ad818ac261069e7f65af77f7f769661
Philip <143160639+philiphekstromer@users.noreply.github.com>
2026-05-12 15:52:10 +0200

    fix: spinnhuset route
    * fix: remove incorrect instruction
    
    * fix: add correct instructions on how to find Spinnhuset

--- src/data/rooms.ts
+++ src/data/rooms.ts
@@ -3,20 +3,23 @@
 
 export const ROOM_DATA_STRUCTURE: Record<string, Record<string, string[]>> = {
   "Huvudentrén": {
-  "Spinnhuset": [
-      "Gå in genom huvudentrén på Plan 1.",
-      "Ta hissen (rakt fram till höger) upp till Plan 4.",
-      "Gå ut ur hissen och ta höger genom den grå branddörren.",
-      "Sväng första vänster vid fikarummet.",
-      "Spinnhuset är den andra dörren på höger sida."
-    ]
   },
     "Personalingång lastkajen": {
     "Spinnhuset": [
       "Ta hissen eller trapporna upp till Plan 4.",
-      "Ta dörren till höger och gå höger mot fikarummet.",
-      "Sväng första vänster vid fikarummet.",
-      "Spinnhuset är den andra dörren på höger sida."
-    ]}
-
+      "Ta dörren till höger och gå åt höger.", 
+      "Gå i korridoren hela vägen till fikarummet i andra änden av huset.",
+      "Sväng höger och gå mot korridoren som är rakt fram.",
+      "Spinnhuset är den första dörren på höger sida när du kommit in i korridoren."
+    ]},
+     "Personalingång": {
+      "Spinnhuset": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå åt höger.",
+      "Gå i korridoren hela vägen till fikarummet i andra änden av huset.",
+      "Sväng vänster och gå rakt fram i korridoren.",
+      "När du kommer till trapphuset tar du antingen vägen höger eller vänster om trapphuset men fortsätter rakt fram.",
+      "Spinnhuset är sista dörren till vänster innan fikarummet."
+    ]
   }
+}
