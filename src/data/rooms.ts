585da849d059f69479fb3b3b40ef44da98b2151b
Philip Hjalmrud Ekströmer <p.ekstromer@gmail.com>
2026-05-13 16:24:32 +0200

    feat: add correct instructions to rooms on floor 4 (north entrance)

--- src/data/rooms.ts
+++ src/data/rooms.ts
@@ -4,22 +4,93 @@
 export const ROOM_DATA_STRUCTURE: Record<string, Record<string, string[]>> = {
   "Huvudentrén": {
   },
-    "Personalingång lastkajen": {
+  "Personalingång lastkajen": {
+    "Varvet": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger.",
+      "Varvet är direkt till vänster."
+    ],
+    "Vattnet": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till vänster.",
+      "Vattnet är direkt till höger."
+    ],
+    "Gripen": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till vänster och gå åt vänster.. Fortsätt rakt fram in i korridoren.",
+      "Gripen är till vänster efter toaletterna."
+    ],
+    "Vintern": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå åt höger. Fortsätt i korridoren hela vägen till fikarummet i andra änden av huset.",
+      "Vintern ligger på vänster sida när du har kommit fram till fikarummet."
+    ],
     "Spinnhuset": [
       "Ta hissen eller trapporna upp till Plan 4.",
-      "Ta dörren till höger och gå åt höger.", 
-      "Gå i korridoren hela vägen till fikarummet i andra änden av huset.",
+      "Ta dörren till höger och gå åt höger. Fortsätt i korridoren hela vägen till fikarummet i andra änden av huset.",
       "Sväng höger och gå mot korridoren som är rakt fram.",
       "Spinnhuset är den första dörren på höger sida när du kommit in i korridoren."
-    ]},
-     "Personalingång": {
-      "Spinnhuset": [
+    ],
+    "Bråviken VK": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och sväng direkt till höger i den första korridoren som går nedåt.",
+      "Följ korridoren rakt fram.",
+      "Bråviken VK ligger på vänster sida precis vid trapphus S82."
+    ],
+    "Roxen VK": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå åt höger. Fortsätt i korridoren hela vägen till fikarummet i andra änden av huset.",
+      "Sväng höger och gå mot korridoren som är rakt fram.",
+      "Fortsätt rakt fram förbi Spinnhuset och mot trapphuset.",
+      "När du kommer till toaletterna tar du antingen vägen höger om trapphuset och fortsätter rakt fram.",
+      "Roxen VK rakt fram när du kommer ut ur korridoren."
+    ],
+    "Jasminen": [
       "Ta hissen eller trapporna upp till Plan 4.",
       "Ta dörren till höger och gå åt höger.",
       "Gå i korridoren hela vägen till fikarummet i andra änden av huset.",
-      "Sväng vänster och gå rakt fram i korridoren.",
-      "När du kommer till trapphuset tar du antingen vägen höger eller vänster om trapphuset men fortsätter rakt fram.",
-      "Spinnhuset är sista dörren till vänster innan fikarummet."
+      "Sväng höger och gå mot korridoren som är rakt fram.",
+      "Fortsätt rakt fram förbi Spinnhuset. När du kommer till toaletterna tar du antingen vägen höger eller vänster om trapphuset men fortsätter rakt fram.",
+      "Fortsätt rakt fram i korridoren tills du kommer till fikarummet.",
+      "Jasminen är dörren på vänster sida direkt när du kommer ut ur korridoren."
+    ],
+    "Ljuset": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå mot höger. Sväng direkt höger och gå mot korridoren.",
+      "Gå rakt fram genom korridoren hela vägen ner till fikarummet på andra sidan huset.",
+      "Sväng vänster i första korridoren, innan miljöstationen",
+      "Ljuset är det första rummet på höger sida efter toaletterna."
+    ],
+    "Källan": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå mot höger. Sväng direkt höger och gå mot korridoren.",
+      "Gå rakt fram genom korridoren hela vägen ner till fikarummet på andra sidan huset.",
+      "Sväng vänster i andra korridoren, efter köket",
+      "Källan är det första rummet på vänster sida efter toaletterna."
+    ],
+    "Bladet": [
+     "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå mot höger. Sväng höger när du kommer ut till öppna ytan.",
+      "Gå rakt fram genom korridoren hela vägen ner till fikarummet på andra sidan huset.",
+      "Sväng vänster i första korridoren, innan miljöstationen",
+      "Bladet ligger på höger sida, efter att du passerat toaletterna och Ljuset."
+    ],
+    "Täppan": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå mot höger. Sväng höger när du kommer ut till öppna ytan.",
+      "Gå rakt fram genom korridoren hela vägen ner till fikarummet på andra sidan huset.",
+      "Sväng vänster i första korridoren, innan miljöstationen",
+      "Täppan ligger längst bort i korridoren, efter att du har passerat toaletterna, Ljuset och Bladet."
+    ],
+    "Spetsen": [
+      "Ta hissen eller trapporna upp till Plan 4.",
+      "Ta dörren till höger och gå mot höger. Sväng direkt höger och gå mot korridoren.",
+      "Gå rakt fram genom korridoren hela vägen ner till fikarummet på andra sidan huset.",
+      "Sväng vänster i andra korridoren, efter köket",
+      "Spetsen ligger på höger sida i slutet av korridoren."
     ]
+  },
+  "Personalingång": {
+    
   }
 }
